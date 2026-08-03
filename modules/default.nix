89c9b76843c94ce593b769663aa7245739149d40
Stylinx <Artemis25265@gmail.com>
2026-08-03 16:35:12 -0400

    localsend 2.0

--- modules/default.nix
+++ modules/default.nix
@@ -7,6 +7,7 @@
     ./nixos/HomeManager.nix
     ./nixos/Keyd.nix
     ./nixos/Fonts.nix
+    ./nixos/LocalSend.nix
   ];
   networking.networkmanager.enable = true;
   time.timeZone = "America/New_York";
